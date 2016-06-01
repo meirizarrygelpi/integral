376bd87d5881d1c93ecfc89caf7452f8f346f6b7
Melvin Eloy Irizarry-Gelpí <melvineloy@gmail.com>
2016-06-01 08:28:15 -0400

    Update Perplex type

--- perplex.go
+++ perplex.go
@@ -6,39 +6,19 @@ package integral
 import (
 	"fmt"
 	"math/big"
+	"math/rand"
+	"reflect"
 	"strings"
 )
 
 // A Perplex represents an integral perplex number.
 type Perplex struct {
-	l, r *big.Int
-}
-
-// L returns the left Cayley-Dickson component of z.
-func (z *Perplex) L() *big.Int {
-	return z.l
-}
-
-// R returns the right Cayley-Dickson component of z.
-func (z *Perplex) R() *big.Int {
-	return z.r
-}
-
-// SetL sets the left Cayley-Dickson component of z equal to a.
-func (z *Perplex) SetL(a *big.Int) {
-	z.l = a
-}
-
-// SetR sets the right Cayley-Dickson component of z equal to b.
-func (z *Perplex) SetR(b *big.Int) {
-	z.r = b
+	l, r big.Int
 }
 
 // Cartesian returns the two cartesian components of z.
-func (z *Perplex) Cartesian() (a, b *big.Int) {
-	a = z.L()
-	b = z.R()
-	return
+func (z *Perplex) Cartesian() (*big.Int, *big.Int) {
+	return &z.l, &z.r
 }
 
 // String returns the string version of a Perplex value.
@@ -48,11 +28,11 @@ func (z *Perplex) Cartesian() (a, b *big.Int) {
 func (z *Perplex) String() string {
 	a := make([]string, 5)
 	a[0] = "("
-	a[1] = fmt.Sprintf("%v", z.L())
-	if z.R().Sign() == -1 {
-		a[2] = fmt.Sprintf("%v", z.R())
+	a[1] = fmt.Sprintf("%v", z.l)
+	if z.r.Sign() == -1 {
+		a[2] = fmt.Sprintf("%v", z.r)
 	} else {
-		a[2] = fmt.Sprintf("+%v", z.R())
+		a[2] = fmt.Sprintf("+%v", z.r)
 	}
 	a[3] = "s"
 	a[4] = ")"
@@ -61,60 +41,59 @@ func (z *Perplex) String() string {
 
 // Equals returns true if y and z are equal.
 func (z *Perplex) Equals(y *Perplex) bool {
-	if z.L().Cmp(y.L()) != 0 || z.R().Cmp(y.R()) != 0 {
+	if z.l.Cmp(&y.l) != 0 || z.r.Cmp(&y.r) != 0 {
 		return false
 	}
 	return true
 }
 
-// Copy copies y onto z, and returns z.
-func (z *Perplex) Copy(y *Perplex) *Perplex {
-	z.SetL(y.L())
-	z.SetR(y.R())
+// Set copies y onto z, and returns z.
+func (z *Perplex) Set(y *Perplex) *Perplex {
+	z.l.Set(&y.l)
+	z.r.Set(&y.r)
 	return z
 }
 
-// NewPerplex returns a pointer to a Perplex value made from two given pointers
-// to big.Int values.
+// NewPerplex returns a pointer to the Perplex value a+bs.
 func NewPerplex(a, b *big.Int) *Perplex {
 	z := new(Perplex)
-	z.SetL(a)
-	z.SetR(b)
+	z.l.Set(a)
+	z.r.Set(b)
 	return z
 }
 
 // Scal sets z equal to y scaled by a, and returns z.
 func (z *Perplex) Scal(y *Perplex, a *big.Int) *Perplex {
-	z.SetL(new(big.Int).Mul(y.L(), a))
-	z.SetR(new(big.Int).Mul(y.R(), a))
+	z.l.Mul(&y.l, a)
+	z.r.Mul(&y.r, a)
 	return z
 }
 
 // Neg sets z equal to the negative of y, and returns z.
 func (z *Perplex) Neg(y *Perplex) *Perplex {
-	z.SetL(new(big.Int).Neg(y.L()))
-	z.SetR(new(big.Int).Neg(y.R()))
+	z.l.Neg(&y.l)
+	z.r.Neg(&y.r)
 	return z
 }
 
 // Conj sets z equal to the conjugate of y, and returns z.
 func (z *Perplex) Conj(y *Perplex) *Perplex {
-	z.SetL(y.L())
-	z.SetR(new(big.Int).Neg(y.R()))
+	z.l.Set(&y.l)
+	z.r.Neg(&y.r)
 	return z
 }
 
 // Add sets z equal to the sum of x and y, and returns z.
 func (z *Perplex) Add(x, y *Perplex) *Perplex {
-	z.SetL(new(big.Int).Add(x.L(), y.L()))
-	z.SetR(new(big.Int).Add(x.R(), y.R()))
+	z.l.Add(&x.l, &y.l)
+	z.r.Add(&x.r, &y.r)
 	return z
 }
 
 // Sub sets z equal to the difference of x and y, and returns z.
 func (z *Perplex) Sub(x, y *Perplex) *Perplex {
-	z.SetL(new(big.Int).Sub(x.L(), y.L()))
-	z.SetR(new(big.Int).Sub(x.R(), y.R()))
+	z.l.Sub(&x.l, &y.l)
+	z.r.Sub(&x.r, &y.r)
 	return z
 }
 
@@ -124,33 +103,39 @@ func (z *Perplex) Sub(x, y *Perplex) *Perplex {
 // 		Mul(s, s) = +1
 // This binary operation is commutative and associative.
 func (z *Perplex) Mul(x, y *Perplex) *Perplex {
-	p := new(Perplex).Copy(x)
-	q := new(Perplex).Copy(y)
-	z.SetL(new(big.Int).Add(
-		new(big.Int).Mul(p.L(), q.L()),
-		new(big.Int).Mul(q.R(), p.R()),
-	))
-	z.SetR(new(big.Int).Add(
-		new(big.Int).Mul(q.R(), p.L()),
-		new(big.Int).Mul(p.R(), q.L()),
-	))
+	a := new(big.Int).Set(&x.l)
+	b := new(big.Int).Set(&x.r)
+	c := new(big.Int).Set(&y.l)
+	d := new(big.Int).Set(&y.r)
+	temp := new(big.Int)
+	z.l.Add(
+		z.l.Mul(a, c),
+		temp.Mul(d, b),
+	)
+	z.r.Add(
+		z.r.Mul(d, a),
+		temp.Mul(b, c),
+	)
 	return z
 }
 
-// Quad returns the quadrance of z, a pointer to a big.Int value.
+// Quad returns the quadrance of z. If z = a+bs, then the quadrance is
+// 		Mul(a, a) - Mul(b, b)
+// This can be positive, negative, or zero.
 func (z *Perplex) Quad() *big.Int {
-	return new(big.Int).Sub(
-		new(big.Int).Mul(z.L(), z.L()),
-		new(big.Int).Mul(z.R(), z.R()),
+	quad := new(big.Int)
+	return quad.Sub(
+		quad.Mul(&z.l, &z.l),
+		new(big.Int).Mul(&z.r, &z.r),
 	)
 }
 
 // IsZeroDiv returns true if z is a zero divisor.
 func (z *Perplex) IsZeroDiv() bool {
-	if z.L().Cmp(z.R()) == 0 {
+	if z.l.Cmp(&z.r) == 0 {
 		return true
 	}
-	if z.L().Cmp(new(big.Int).Neg(z.R())) == 0 {
+	if z.l.Cmp(new(big.Int).Neg(&z.r)) == 0 {
 		return true
 	}
 	return false
@@ -165,7 +150,16 @@ func (z *Perplex) Quo(x, y *Perplex) *Perplex {
 	quad := y.Quad()
 	z.Conj(y)
 	z.Mul(x, z)
-	z.SetL(new(big.Int).Quo(z.L(), quad))
-	z.SetR(new(big.Int).Quo(z.R(), quad))
+	z.l.Quo(&z.l, quad)
+	z.r.Quo(&z.r, quad)
 	return z
+}
+
+// Generate returns a random Perplex value for quick.Check testing.
+func (z *Perplex) Generate(rand *rand.Rand, size int) reflect.Value {
+	randomPerplex := &Perplex{
+		*big.NewInt(rand.Int63()),
+		*big.NewInt(rand.Int63()),
+	}
+	return reflect.ValueOf(randomPerplex)
 }
